2d32c948d916b1c167f588fea93300e100e2aea1
Ubuntu <ubuntu@ip-172-31-95-112.ec2.internal>
2022-12-02 21:55:46 +0000

    Hello, world

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow connections to local server on cloud IDE.
+  config.hosts << "7bbf77ceb3d5479899000e0c72457dba.vfs.cloud9.us-east-1.amazonaws.com"
 end
